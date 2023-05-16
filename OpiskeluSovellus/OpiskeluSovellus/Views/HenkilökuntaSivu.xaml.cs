416421fb0724dce3d50ec2e71244bf1f0db49eb1
Essi Kipinoinen <essi.kipinoinen@student.careeria.fi>
2023-05-16 12:56:42 +0300

    Luokat-sivu ja Henkilökunta-sivut viimeistelyä vaille valmiit

--- OpiskeluSovellus/OpiskeluSovellus/Views/HenkilökuntaSivu.xaml.cs
+++ OpiskeluSovellus/OpiskeluSovellus/Views/HenkilökuntaSivu.xaml.cs
@@ -1,16 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
+using OpiskeluSovellus.Models;
+using System.Collections.ObjectModel;
+using System.Net.Http;
 using Xamarin.Forms;
-
 namespace OpiskeluSovellus
-{	
-	public partial class HenkilökuntaSivu : ContentPage
-	{	
-		public HenkilökuntaSivu ()
-		{
-			InitializeComponent ();
-		}
-	}
+{
+    public partial class HenkilökuntaSivu : ContentPage
+	{
+        ObservableCollection<Henkilökunta> dataa = new ObservableCollection<Henkilökunta>();
+        public HenkilökuntaSivu()
+        {
+            InitializeComponent();
+
+            LoadDataFromRestAPI();
+
+
+            async void LoadDataFromRestAPI()
+            {
+                try
+                {
+                    HttpClientHandler GetInsecureHandler()
+                    {
+                        HttpClientHandler handler = new HttpClientHandler();
+                        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+                        {
+                            if (cert.Issuer.Equals("CN=localhost"))
+                                return true;
+                            return errors == System.Net.Security.SslPolicyErrors.None;
+                        };
+                        return handler;
+                    }
+#if DEBUG
+                    HttpClientHandler insecureHandler = GetInsecureHandler();
+                    HttpClient client = new HttpClient(insecureHandler);
+#else
+                    HttpClient client = new HttpClient();
+#endif
+
+                    client.BaseAddress = new Uri("https://10.0.2.2:7160/");
+                    string json = await client.GetStringAsync("api/henkilökunta");
+
+                    IEnumerable<Henkilökunta> henkilökuntas = JsonConvert.DeserializeObject<Henkilökunta[]>(json);
+                    ObservableCollection<Henkilökunta> dataa2 = new ObservableCollection<Henkilökunta>(henkilökuntas);
+                    dataa = dataa2;
+
+                    henkilökuntalista.ItemsSource = dataa;
+
+                }
+                catch (Exception e)
+                {
+                    await DisplayAlert("Virhe", e.Message.ToString(), "Ok");
+                }
+
+            }
+        }
+    }
 }
 
--- OpiskeluSovellus/OpiskeluSovellus/Views/LuokatSivu.xaml.cs
+++ OpiskeluSovellus/OpiskeluSovellus/Views/LuokatSivu.xaml.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using OpiskeluSovellus.Models;
+using System.Collections.ObjectModel;
+using System.Net.Http;
 using Xamarin.Forms;
 
 namespace OpiskeluSovellus
 {	
 	public partial class LuokatSivu : ContentPage
-	{	
-		public LuokatSivu ()
-		{
-			InitializeComponent ();
-		}
-	}
+	{
+        ObservableCollection<Luokka> dataa = new ObservableCollection<Luokka>();
+        public LuokatSivu()
+        {
+            InitializeComponent();
+
+            LoadDataFromRestAPI();
+
+
+            async void LoadDataFromRestAPI()
+            {
+                try
+                {
+                    HttpClientHandler GetInsecureHandler()
+                    {
+                        HttpClientHandler handler = new HttpClientHandler();
+                        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+                        {
+                            if (cert.Issuer.Equals("CN=localhost"))
+                                return true;
+                            return errors == System.Net.Security.SslPolicyErrors.None;
+                        };
+                        return handler;
+                    }
+#if DEBUG
+                    HttpClientHandler insecureHandler = GetInsecureHandler();
+                    HttpClient client = new HttpClient(insecureHandler);
+#else
+                    HttpClient client = new HttpClient();
+#endif
+
+                    client.BaseAddress = new Uri("https://10.0.2.2:7160/");
+                    string json = await client.GetStringAsync("api/luokat");
+
+                    IEnumerable<Luokka> luokat = JsonConvert.DeserializeObject<Luokka[]>(json);
+                    ObservableCollection<Luokka> dataa2 = new ObservableCollection<Luokka>(luokat);
+                    dataa = dataa2;
+
+                    luokkalista.ItemsSource = dataa;
+
+                }
+                catch (Exception e)
+                {
+                    await DisplayAlert("Virhe", e.Message.ToString(), "Ok");
+                }
+
+            }
+        }
+    }
 }
 
